1a39ed807ebc67aee824493b1b08a1b6dbf320f0
unknown <you@example.com>
2024-03-19 12:47:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import './App.css';
+import Home from './Components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import services from './Components/pages/Services';
+import Products from './Components/pages/Products';
+import SignUp from './Components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home} />
+          <Route path='/services' Component={services} />
+          <Route path='/products' Component={Products} />
+          <Route path='/sign-up' Component={SignUp} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
