dec9b0a2e0198973ef0eae86ab6b21af92315f5c
unknown <you@example.com>
2024-03-23 22:45:09 +0530

    Updates

---
+++ precache-manifest.d87540a75b64125929a0d3c4a45cc000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e649bb46192aa29e5ac62666ca325fb",
+    "url": "/module/index.html"
+  },
+  {
+    "revision": "a2d7fe1f494319b2665a",
+    "url": "/module/static/css/main.4258301e.chunk.css"
+  },
+  {
+    "revision": "5b2e39b31618c4f0b134",
+    "url": "/module/static/js/2.47967f1c.chunk.js"
+  },
+  {
+    "revision": "a2d7fe1f494319b2665a",
+    "url": "/module/static/js/main.7ce854a9.chunk.js"
+  },
+  {
+    "revision": "62f84b8edc1de9929713",
+    "url": "/module/static/js/runtime~main.c2aa284c.js"
+  }
+]);
